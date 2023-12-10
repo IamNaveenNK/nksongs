0d9568ed6711c71f78bcb8ab6a4ef87190f58603
IamNaveenNK <153352759+IamNaveenNK@users.noreply.github.com>
2023-12-10 07:28:30 -0800

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,70 @@
+let allMusic=[
+{
+    name:"Naruto - Theme",
+    artist:"Masashi Kishimoto",
+    img:"narutouzu",
+    src:"narutoTheme"
+},
+{
+    name:"Naruto - Blue Bird",
+    artist:"Masashi Kishimoto",
+    img:"blubird",
+    src:"blue bird"
+},
+{
+    name:"Itachi Uchiha",
+    artist:"Masashi Kishimoto",
+    img:"itachi",
+    src:"itachisad"
+},
+{
+    name:"Itachi - Sasuke...!",
+    artist:"Masashi Kishimoto",
+    img:"itachi-sasuke",
+    src:"itachi_sasuke"
+},
+{
+    name:"Akatshuki Theme",
+    artist:"Masashi Kishimoto",
+    img:"akatshuki",
+    src:"akatsuki"
+},
+{
+    name:"Akatshuki Group",
+    artist:"Masashi Kishimoto",
+    img:"akatshukiGroup",
+    src:"akatshukigroup"
+},
+{
+    name:"Akatshuki Name",
+    artist:"Masashi Kishimoto",
+    img:"team akatshuki",
+    src:"akatshukiName"
+    
+},
+{
+    name:"Pain Theme",
+    artist:"Masashi Kishimoto",
+    img:"pain-3",
+    src:"paintheme"
+},
+{
+    name:"Sasuke NANDOMO",
+    artist:"Masashi Kishimoto",
+    img:"sasuke",
+    src:"NANDOMO"
+},
+{
+    name:"Madara",
+    artist:"Masashi Kishimoto",
+    img:"madara",
+    src:"genjutsu"
+},
+{
+    name:"Wake up to Reality - MADARA",
+    artist:"Masashi Kishimoto",
+    img:"images (57)",
+    src:"madara"
+}
+
+];
